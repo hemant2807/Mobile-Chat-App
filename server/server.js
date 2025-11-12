a53b3426744df855999c34c8607ef96b513a0448
Hemant Kumar <hemant.k2807@gmail.com>
2025-11-12 20:32:50 +0530

    Initialize express server

---
+++ server/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
