6824f2a5c0087aebf42a6baf151e26d7b2b1f519
Hemant Kumar <hemant.k2807@gmail.com>
2025-11-12 22:39:32 +0530

    added conversation and message models

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(cookieParser());
